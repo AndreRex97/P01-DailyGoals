5d920f92f8aadbc7e4d369e0b2cba02217a51213
15017103 <15017103@myrp.edu.sg>
2017-04-20 16:34:21 +0800

    Initial Commit

--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Main2Activity.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/Main2Activity.java
@@ -3,6 +3,8 @@ package sg.edu.rp.c346.p01_dailygoals;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
@@ -11,6 +13,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button btnDone = (Button) findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         Intent i = getIntent();
         // Get the String array named "info" we passed in
         String[] info = i.getStringArrayExtra("info");
@@ -19,7 +28,7 @@ public class Main2Activity extends AppCompatActivity {
         // Display the name and age on the TextView
         tv1.setText("Read up on materials before class: " + info[0]
                 + "\nArrive on time so as not to miss important part of the lesson: " + info[1]
-        + "\n Attempt the problem myself: " + info[2]
-        + "\n Reflection: " + info[3]);
+                + "\n Attempt the problem myself: " + info[2]
+                + "\n Reflection: " + info[3]);
     }
 }
--- app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p01_dailygoals/MainActivity.java
@@ -1,6 +1,8 @@
 package sg.edu.rp.c346.p01_dailygoals;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,28 +15,29 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup RG1, RG2, RG3;
     EditText etReflection;
     Button btnOK;
+    RadioButton rb1, rb2, rb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RG1 = (RadioGroup) findViewById(R.id.RadioGroup1);
+        RG2 = (RadioGroup) findViewById(R.id.RadioGroup2);
+        RG3 = (RadioGroup) findViewById(R.id.RadioGroup3);
+        etReflection = (EditText) findViewById(R.id.editTextReflection);
 
         btnOK = (Button) findViewById(R.id.ButtonOK);
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RG1 = (RadioGroup) findViewById(R.id.RadioGroup1);
-                RG2 = (RadioGroup) findViewById(R.id.RadioGroup2);
-                RG3 = (RadioGroup) findViewById(R.id.RadioGroup3);
-                etReflection = (EditText) findViewById(R.id.editTextReflection);
 
                 int RB1 = RG1.getCheckedRadioButtonId();
                 int RB2 = RG2.getCheckedRadioButtonId();
                 int RB3 = RG3.getCheckedRadioButtonId();
 
-                RadioButton rb1 = (RadioButton) findViewById(RB1);
-                RadioButton rb2 = (RadioButton) findViewById(RB2);
-                RadioButton rb3 = (RadioButton) findViewById(RB3);
+                rb1 = (RadioButton) findViewById(RB1);
+                rb2 = (RadioButton) findViewById(RB2);
+                rb3 = (RadioButton) findViewById(RB3);
 
                 String[] info = {rb1.getText().toString(), rb2.getText().toString(), rb3.getText().toString(), etReflection.getText().toString()};
 
@@ -46,7 +49,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        int rbFirst = RG1.getCheckedRadioButtonId();
+        int rbSecond = RG2.getCheckedRadioButtonId();
+        int rbThird = RG3.getCheckedRadioButtonId();
+        String strReflection = etReflection.getText().toString();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putInt("first", rbFirst);
+        prefEdit.putInt("second", rbSecond);
+        prefEdit.putInt("third", rbThird);
+        prefEdit.putString("reflection", strReflection);
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+        int strFirst = prefs.getInt("first", 0);
+        int strSecond = prefs.getInt("second", 0);
+        int strThird = prefs.getInt("third", 0);
+        String strReflection = prefs.getString("reflection", "");
+
+        etReflection.setText(strReflection);
+        RG1.check(strFirst);
+        RG2.check(strSecond);
+        RG3.check(strThird);
     }
 }
